b5d473cd9fcdca689e3f4a46b53608a6e28e3f39
Vladyslav Biriukov <vladislavbiriukovsquirrel@gmail.com>
2023-09-17 21:30:18 +0200

    Update filter.js

--- src/components/SearchForm/filter.js
+++ src/components/SearchForm/filter.js
@@ -1,150 +1,16 @@
 export const filterCar = (search, array) => {
   const { brand, price, minMileage, maxMileage } = search;
 
-  if (brand !== "" && price !== "" && minMileage !== "" && maxMileage !== "") {
-    const filtred = array
-      .filter((item) => item.make === brand)
-      .map((item) => ({
-        ...item,
-        rentalPrice: Number(item.rentalPrice.slice(1, item.rentalPrice.length)),
-      }))
-      .filter((item) => item.rentalPrice <= price)
-      .map((item) => ({ ...item, rentalPrice: "$" + item.rentalPrice }))
-      .filter(
-        (item) => item.mileage >= minMileage && item.mileage <= maxMileage
-      );
-    return filtred;
-  }
-
-  if (brand !== "" && price !== "" && minMileage !== "" && maxMileage === "") {
-    const filtred = array
-      .filter((item) => item.make === brand)
-      .map((item) => ({
-        ...item,
-        rentalPrice: Number(item.rentalPrice.slice(1, item.rentalPrice.length)),
-      }))
-      .filter((item) => item.rentalPrice <= price)
-      .map((item) => ({ ...item, rentalPrice: "$" + item.rentalPrice }))
-      .filter((item) => item.mileage >= minMileage);
-    return filtred;
-  }
-
-  if (brand !== "" && price !== "" && minMileage === "" && maxMileage !== "") {
-    const filtred = array
-      .filter((item) => item.make === brand)
-      .map((item) => ({
-        ...item,
-        rentalPrice: Number(item.rentalPrice.slice(1, item.rentalPrice.length)),
-      }))
-      .filter((item) => item.rentalPrice <= price)
-      .map((item) => ({ ...item, rentalPrice: "$" + item.rentalPrice }))
-      .filter((item) => item.mileage <= maxMileage);
-    return filtred;
-  }
-
-  if (brand !== "" && price === "" && minMileage !== "" && maxMileage !== "") {
-    const filtred = array
-      .filter((item) => item.make === brand)
-      .filter(
-        (item) => item.mileage >= minMileage && item.mileage <= maxMileage
-      );
-    return filtred;
-  }
-
-  if (brand === "" && price !== "" && minMileage !== "" && maxMileage !== "") {
-    const filtred = array
-      .map((item) => ({
-        ...item,
-        rentalPrice: Number(item.rentalPrice.slice(1, item.rentalPrice.length)),
-      }))
-      .filter((item) => item.rentalPrice <= price)
-      .map((item) => ({ ...item, rentalPrice: "$" + item.rentalPrice }))
-      .filter(
-        (item) => item.mileage >= minMileage && item.mileage <= maxMileage
-      );
-    return filtred;
-  }
-
-  if (brand !== "" && price !== "" && minMileage === "" && maxMileage === "") {
-    const filtred = array
-      .filter((item) => item.make === brand)
-      .map((item) => ({
-        ...item,
-        rentalPrice: Number(item.rentalPrice.slice(1, item.rentalPrice.length)),
-      }))
-      .filter((item) => item.rentalPrice <= price)
-      .map((item) => ({ ...item, rentalPrice: "$" + item.rentalPrice }));
-    return filtred;
-  }
-
-  if (brand !== "" && price === "" && minMileage !== "" && maxMileage === "") {
-    const filtred = array
-      .filter((item) => item.make === brand)
-      .filter((item) => item.mileage >= minMileage);
-    return filtred;
-  }
-
-  if (brand === "" && price !== "" && minMileage !== "" && maxMileage === "") {
-    const filtred = array
-      .map((item) => ({
-        ...item,
-        rentalPrice: Number(item.rentalPrice.slice(1, item.rentalPrice.length)),
-      }))
-      .filter((item) => item.rentalPrice <= price)
-      .map((item) => ({ ...item, rentalPrice: "$" + item.rentalPrice }))
-      .filter((item) => item.mileage >= minMileage);
-    return filtred;
-  }
-
-  if (brand !== "" && price === "" && minMileage === "" && maxMileage !== "") {
-    const filtred = array
-      .filter((item) => item.make === brand)
-      .filter((item) => item.mileage <= maxMileage);
-    return filtred;
-  }
-
-  if (brand === "" && price !== "" && minMileage === "" && maxMileage !== "") {
-    const filtred = array
-      .map((item) => ({
-        ...item,
-        rentalPrice: Number(item.rentalPrice.slice(1, item.rentalPrice.length)),
-      }))
-      .filter((item) => item.rentalPrice <= price)
-      .map((item) => ({ ...item, rentalPrice: "$" + item.rentalPrice }))
-      .filter((item) => item.mileage <= maxMileage);
-    return filtred;
-  }
-
-  if (brand === "" && price === "" && minMileage !== "" && maxMileage !== "") {
-    const filtred = array.filter(
-      (item) => item.mileage >= minMileage && item.mileage <= maxMileage
-    );
-    return filtred;
-  }
-
-  if (brand !== "" && price === "" && minMileage === "" && maxMileage === "") {
-    const filtred = array.filter((item) => item.make === brand);
-    return filtred;
-  }
-
-  if (brand === "" && price !== "" && minMileage === "" && maxMileage === "") {
-    const filtred = array
-      .map((item) => ({
-        ...item,
-        rentalPrice: Number(item.rentalPrice.slice(1, item.rentalPrice.length)),
-      }))
-      .filter((item) => item.rentalPrice <= price)
-      .map((item) => ({ ...item, rentalPrice: "$" + item.rentalPrice }));
-    return filtred;
-  }
-
-  if (brand === "" && price === "" && minMileage !== "" && maxMileage === "") {
-    const filtred = array.filter((item) => item.mileage >= minMileage);
-    return filtred;
-  }
-
-  if (brand === "" && price === "" && minMileage === "" && maxMileage !== "") {
-    const filtred = array.filter((item) => item.mileage <= maxMileage);
-    return filtred;
-  }
+  return array
+    .filter(item => {
+      if (brand && item.make !== brand) return false;
+      if (price && Number(item.rentalPrice.slice(1)) > price) return false;
+      if (minMileage && item.mileage < minMileage) return false;
+      if (maxMileage && item.mileage > maxMileage) return false;
+      return true;
+    })
+    .map(item => ({
+      ...item,
+      rentalPrice: "$" + Number(item.rentalPrice.slice(1))
+    }));
 };
