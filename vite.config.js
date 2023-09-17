5043e53078b639f79bd05b4575a42fd14565b8a0
Vladyslav Biriukov <vladislavbiriukovsquirrel@gmail.com>
2023-09-17 20:26:56 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,49 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig(({ command }) => {
+//   const config = {
+//     plugins: [react()],
+//     base: "/",
+//     css: {
+//       preprocessorOptions: {
+//         scss: {
+//           additionalData: '@import "./src/styles/_shared.scss";',
+//         },
+//       },
+//     },
+//   };
+//   if (command !== "serve") {
+//     config.base = "/car-rent/";
+//   }
+//   return config;
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig(({ command }) => {
   const config = {
-    plugins: [react()],
-    base: "/",
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: '@import "./src/styles/_shared.scss";',
-        },
+    base: '/',
+    resolve: {
+      alias: {
+        src: '/src',
+        components: '/src/components',
+        pages: '/src/pages',
+        services: '/src/services',
+        styles: '/src/styles',
+        images: '/src/images',
+        assets: '/src/assets',
+        data: '/src/data',
       },
     },
+    plugins: [react()],
   };
-  if (command !== "serve") {
-    config.base = "/car-rent/";
+
+  if (command !== 'serve') {
+    config.base = '/car-ren/';
   }
+
   return config;
 });
