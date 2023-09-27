1f1bd971d1637d0b88d377d61fb621727ee7ba9a
Vladyslav Biriukov <vladislavbiriukovsquirrel@gmail.com>
2023-09-27 13:06:30 +0200

    Updates

--- src/pages/Catalog.jsx
+++
@@ -1,9 +0,0 @@
-import { CarCatalog } from "../components/CarCatalog/CarCatalog";
-
-export const Catalog = () => {
-  return (
-    <div>
-      <CarCatalog />
-    </div>
-  );
-};
--- src/pages/ErrorPage.jsx
+++
@@ -1,8 +0,0 @@
-// import { Error } from '../components/Error/Error';
-export const ErrorPage = () => {
-  return (
-    <section>
-      <h2>ErrorPage</h2>
-    </section>
-  );
-};
